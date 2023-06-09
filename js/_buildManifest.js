676e66f87a2103fae430bc9684b321d672f1f817
J-Bankman <131815484+J-Bankman@users.noreply.github.com>
2023-06-09 05:12:17 +0200

    Add files via upload

---
+++ js/_buildManifest.js
@@ -0,0 +1,14 @@
+(self.__BUILD_MANIFEST = (function (s, e, a) {
+  return {
+    __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+    "/": [s, e, a, "static/chunks/pages/index-c11390b3d9ae1f10.js"],
+    "/_error": ["static/chunks/pages/_error-82b79221b9ed784b.js"],
+    "/home": [s, e, a, "static/chunks/pages/home-229fee644a636898.js"],
+    sortedPages: ["/", "/_app", "/_error", "/home"],
+  };
+})(
+  "static/chunks/9929-36ba091da90a170b.js",
+  "static/chunks/2095-8c5f3d805de64980.js",
+  "static/css/46293554031b7c00.css"
+)),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
---
+++ js/index-c11390b3d9ae1f10.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5405],{48312:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(70085)}])},70085:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return e}});var t=_(42095),e=!0;u.default=t.default}},function(n){n.O(0,[9929,2095,9774,2888,179],(function(){return n(n.s=48312)})),_N_E=n.O()}]);
